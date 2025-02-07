2269f499ed7100801000bac5c5277e88d9f928dc
Pedro Lima <pedrolimaa354@gmail.com>
2025-02-06 21:46:48 -0300

    feat: Implementação das bibliotecas necessárias.

---
+++ Main.py
@@ -0,0 +1,7 @@
+# Nesta etapa, estou importando as bibliotecas necessárias para fazer todo o código funcionar.
+import easyocr
+import pandas
+import os
+import tkinter as tk
+from tkinter import filedialog
+
