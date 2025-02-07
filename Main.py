453dcfb5c7d33e9c2c76267fe1ce79418b36c5bb
Pedro Lima <pedrolimaa354@gmail.com>
2025-02-06 22:51:46 -0300

    feat: Adicionando as funções para leitura de imagens e PDF.

--- Main.py
+++ Main.py
@@ -7,3 +7,31 @@ from tkinter import filedialog
 import PyPDF2
 import fitz  # PyMuPDF
 
+
+# Adicionando as funções que usarei para realizar a leitura da imagem
+def selecionar_imagens():
+    root = tk.Tk()
+    root.withdraw()
+    caminho_imagens = filedialog.askopenfilenames(filetypes=[("Imagens", "*.png; *.jpg; *.jpeg; *.pdf")]) #aqui, podemos alterar para o código receber as imagens que desejamos
+    return caminho_imagens # retornamos o nosso caminho de imagens
+
+def selecionar_pdf(caminho_pdf):
+    texto = ""
+    doc = fitz.open(caminho_pdf)
+    for pagina in doc:
+        texto += pagina.get_text() # Aqui, é extraído o texto de cada imagem
+    return texto  
+
+def processamento(arquivos):
+    for arquivos in arquivos:
+        if arquivos.lower().endswith(('png', 'jpg', 'jpeg')):
+            resultado = leitura.readtext(arquivo)
+            for item in resultado:
+                print(f"Texto extraído da imagem: {item[1]}")
+        elif arquivos.lower().endswhith('pdf'):
+            texto_pdf = selecionar_pdf(arquivo)
+            print(f"Texto extraído do PDF: \n{texto_pdf}")
+        else:
+            print(f"Arquivo {arquivos} não suportado")
+
+
